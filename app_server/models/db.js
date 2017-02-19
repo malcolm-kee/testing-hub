a3852bd60c5e009601ac2187ef999b411a070204
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2017-02-19 22:12:27 +0800

    update styling of navigation bar hover

---
+++ app_server/models/db.js
@@ -0,0 +1,39 @@
+var mongoose = require('mongoose');
+var dbURI = 'mongodb://localhost/DSOPortal';
+
+mongoose.connect(dbURI);
+
+mongoose.connection.on('connected', function() {
+	console.log('Mongoose connect to ' + dbURI);
+});
+mongoose.connection.on('error', function(err) {
+	console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function() {
+	console.log('Mongoose disconnected');
+});
+
+function gracefulShutdown(msg, callback) {
+	mongoose.connection.close(function() {
+		console.log('Mongoose disconnected through ' + msg);
+		callback();
+	});
+}
+
+process.once('SIGUSR2', function() {
+	gracefulShutdown('nodemon restart', function() {
+		process.kill(process.pid, 'SIGUSR2');
+	});
+});
+
+process.on('SIGINT', function() {
+	gracefulShutdown('app termination', function() {
+		process.exit(0);
+	});
+});
+
+process.on('SIGTERM', function() {
+	gracefulShutdown('Heroku app shutdown', function() {
+		process.exit(0);
+	});
+});
