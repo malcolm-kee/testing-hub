2b69b35e39ea2bf02a0b41857e17d33fe36e6f7b
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2017-02-22 01:10:22 +0800

    remove https prefix

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -3,7 +3,7 @@ var apiOptions = {
 	server: "http://localhost:3000"
 };
 if (process.env.NODE_ENV === 'production') {
-	apiOptions.server = 'https://vast-journey-10806.herokuapp.com';
+	apiOptions.server = 'vast-journey-10806.herokuapp.com';
 }
 
 var filterListByCategories = function(list, category) {
