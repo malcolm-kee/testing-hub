7e2b1ed8a043d9f47575258797ef25f0bc0c5f9f
Malcolm Kee <wee.siong.kee@accenture.com>
2017-11-12 11:27:51 +0800

    added redux combineReducers implementation comment

--- client/src/reducers.js
+++ client/src/reducers.js
@@ -1,5 +1,14 @@
 import { combineReducers } from 'redux';
 
+// self-implemented combineReducers based on lesson on https://egghead.io/lessons/javascript-redux-implementing-combinereducers-from-scratch
+/*
+const combineReducers = reducers => (state = {}, action) =>
+	Object.keys(reducers).reduce(
+		(nextState, key) => Object.assign(nextState, { [key]: reducers[key](state[key], action) }),
+		{}
+	);
+*/
+
 import userReducer from './userReducer';
 import featureReducer from './featureReducer';
 import sprintReducer from './sprintReducer';
