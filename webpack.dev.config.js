e01c0a27d5d10185665f9a9071533eda4dc1b3c4
Malcolm Kee <wee.siong.kee@accenture.com>
2017-11-23 00:02:02 +0800

    change webpack watch config

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,14 +1,14 @@
-const path = require('path');
-const webpack = require('webpack');
+/* const path = require('path');
+const webpack = require('webpack');*/
 const merge = require('webpack-merge');
 const prodconfig = require('./webpack.config.js');
 
 module.exports = merge.strategy({
 	entry: 'prepend'
 })(prodconfig, {
-	entry: ['react-hot-loader/patch', 'webpack-dev-server/client?http://localhost:3000', 'webpack/hot/only-dev-server'],
-	devtool: 'source-map',
-	devServer: {
+	// entry: ['react-hot-loader/patch', 'webpack-dev-server/client?http://localhost:3000', 'webpack/hot/only-dev-server'],
+	devtool: 'source-map'
+	/* devServer: {
 		hot: true,
 		port: 3000,
 		contentBase: path.resolve('public'),
@@ -18,5 +18,5 @@ module.exports = merge.strategy({
 		},
 		historyApiFallback: true
 	},
-	plugins: [new webpack.HotModuleReplacementPlugin(), new webpack.NamedModulesPlugin()]
+	plugins: [new webpack.HotModuleReplacementPlugin(), new webpack.NamedModulesPlugin()] */
 });
