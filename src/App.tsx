3f5e790fcb2a475531dd54c55120da9f4b68ca9f
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2017-11-26 16:17:41 +0800

    prettier and ts config for migration to typescript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+// import { Provider } from 'react-redux';
+// import store from './store';
+// import MainRoute from './MainRoute';
+
+class App extends React.Component {
+  public render() {
+    return <div>Hello world from TS!</div>;
+  }
+}
+
+export default App;
