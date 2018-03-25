66aba8c09c986065eb3574b55dde0c5948aa6777
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2018-03-25 13:30:59 +0800

    added webpack-pwa-manifest plugin

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const I18nPlugin = require('i18n-webpack-plugin');
+const WebpackPwaManifestPlugin = require('webpack-pwa-manifest');
 const ProgressBarPlugin = require('progress-bar-webpack-plugin');
 
 const enConfig = require('./i18n/en.json');
@@ -65,6 +66,22 @@ module.exports = {
       }
     ]),
     new ExtractTextPlugin('style.css'),
+    new WebpackPwaManifestPlugin({
+      name: 'Testing Hub',
+      short_name: 'Testing Hub',
+      lang: 'en-US',
+      description: 'An app to manage your testing',
+      background_color: '#3498db',
+      theme_color: '#3498db',
+      ios: true,
+      icons: [
+        {
+          src: path.resolve('public/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512, 1024],
+          type: 'image/png'
+        }
+      ]
+    }),
     new ProgressBarPlugin()
   ]
 };
